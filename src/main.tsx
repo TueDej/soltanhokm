12eb896051c2067dc9c0c238f4686e58daef9bf9
TueDej <soweeeso@gmail.com>
2026-06-24 16:40:11 +0330

    Read version from package.json instead of hardcoding

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+document.documentElement.style.setProperty('--app-version', `'v${__APP_VERSION__}'`)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
